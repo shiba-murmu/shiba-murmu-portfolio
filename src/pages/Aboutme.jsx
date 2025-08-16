46a2972008e3f511c0c76d4d3813f00a71712f59
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-16 10:38:46 +0530

    some colors changed .

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -10,7 +10,7 @@ function Aboutme() {
             </div>
             <div className='flex flex-col justify-center items-start md:w-1/2 p-5'>
                 {/* for heading */}
-                <span className='text-2xl md:text-4xl font-bold border-l-1 px-4 py-2 rounded-r-md bg-[#efefef]'>About Me</span>
+                <span className='text-2xl md:text-4xl font-bold border-l-1 px-4 py-2 rounded-r-md bg-[#98197c]'>About Me</span>
                 <div>
                     <p className='mt-4 text-md md:text-lg'>I am a Computer Science graduate (B.Tech, 2025) with strong skills in Python, Django, and React.js. I enjoy building dynamic, user-friendly web applications and have completed several projects combining frontend and backend development. Passionate about problem-solving and eager to learn new technologies.</p>
                     <p className='mt-4 text-md md:text-lg'> My journey in web development began with a curiosity for how websites work, and it has since evolved into a full-fledged career.</p>
