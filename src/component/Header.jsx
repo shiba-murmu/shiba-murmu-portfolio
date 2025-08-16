97dd1f4c525ec79a3f6ad3a1199fc6ad1f0a2a68
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-16 10:47:17 +0530

    say hello button bg-color changed.

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,12 +15,12 @@ function Header() {
                 <div className='hidden md:w-1/2 md:flex gap-3 items-center justify-end mr-10'>
                     <div>Web developer</div>
                     <Link to={'/contact'}>
-                        <span className='text-md border rounded-4xl p-1.5 md:px-6 hover:cursor-pointer hover:bg-[#6806ed] hover:border-[white] hover:text-white'>Say Hello</span>
+                        <span className='text-md border rounded-4xl p-1.5 md:px-6 hover:cursor-pointer hover:bg-[#98197c] hover:border-[white] hover:text-white'>Say Hello</span>
                     </Link>
                 </div>
                 <div>
                     <Link to={'/contact'} className='md:hidden'>
-                        <span className='border md:hidden text-sm rounded-4xl p-2 px-5 hover:cursor-pointer hover:bg-[#6806ed] hover:border-[white] hover:text-white'>Say hello</span>
+                        <span className='border md:hidden text-sm rounded-4xl p-2 px-5 hover:cursor-pointer hover:bg#98197c] hover:border-[white] hover:text-white'>Say hello</span>
                     </Link>
                 </div>
             </div>
