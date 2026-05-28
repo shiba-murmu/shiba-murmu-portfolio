7aecdfd9c66a5276800a2cfc58aa218cd718bc7e
Shiba Murmu <shibamurmu001@gmail.com>
2026-05-28 09:50:35 +0530

    starting from 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import React from 'react'
-import Container from './container/Container'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Contact from './pages/Contact'
-import Admin from './pages/admin/Admin'
-import { ToastContainer } from 'react-toastify'
+
 function App() {
   return (
-    <>
-        <Router>
-            <ToastContainer />
-            <Routes>
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/admin' element={<Admin />} />
-                <Route path="/" element={<Container />} />
-            </Routes>
-        </Router>
-    </>
+    <div>App</div>
   )
 }
 
