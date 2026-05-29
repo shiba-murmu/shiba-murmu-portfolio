42290163a68ee6ab5a31a7d8d2c6f232f9ae632b
Shiba Murmu <shibamurmu001@gmail.com>
2026-05-29 06:06:57 +0530

    Lenis file is designed.

---
+++ src/hooks/useLenis.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+}
